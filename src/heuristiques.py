bf45d8b4ffbaee13b26cf6756e32ca3013921665
romaric <romaric@romaric-Lenovo-ideapad-500-15ISK>
2019-10-23 18:18:19 +0200

    Ajout d'une nouvelle heuristique qui prend des objets déjà rangés pour les placer ailleurs.

--- src/heuristiques.py
+++ src/heuristiques.py
@@ -1,5 +1,22 @@
-from classes import Boite , Objet , chargerBench
 import random
+from time import clock
+
+from classes import Boite , Objet , chargerBench
+
+
+def permuter_objets(boite1 , boite2 , obj1 , obj2):
+    """Permute 2 objets entre 2 boites. Attention , il faut que les contraintes de poids et de couleur
+    soient toujours respectés."""
+    boite1.supprimerObjet(obj1)
+    boite2.supprimerObjet(obj2)
+    
+    if (boite1.ajouterObjet(obj2) == False):
+        boite1.ajouterObjet(obj1)
+        boite2.ajouterObjet(obj2)
+    elif (boite2.ajouterObjet(obj1) == False):
+        boite1.supprimerObjet(obj2)
+        boite1.ajouterObjet(obj1)
+        boite2.ajouterObjet(obj2)
 
 def objet_le_plus_lourd(listeObj):
     """Retourne l'objet de poids le plus élevé parmi la liste d'objets."""
@@ -134,8 +151,69 @@ def heur_naive_trie_selon_poids(listeObj , capBoites):
         
     return (listeBoites , sumCapResid)
         
+def heur_naive_trie_selon_poids_depl_obj(listeObj , capBoites , tempsLimite=1):
+    """Même heuristique que précédemment sauf que l'étape de placer les objets de taille égale à un capacité d'une boite ; a été retirée
+    et les objets sont triés selon leur poids par ordre décroissant.
+    Ensuite on essaie de transférer des objets dans des boites qui pourraient être comblées par ces objets là.
+    Et on redimensionne (si possible) la boite à qui on a volé un objet par un des types de boites existants afin de réduire la 
+    capacité perdue. La limite de temps est en secondes. (par défaut = 1 seconde)"""
+    
+    tempsDebut = clock()
+    
+    listeBoites = []
+    listeObj2 = listeObj.copy() # Copie de la liste d'objets afin de ne pas altérer la liste originale
+    i = 0 # itérateur pour l'id des boites
+    listeObj2 = trier_objets_selon_poids(listeObj2)
+    while listeObj2 != []:
+        sumPoids = 0 # Somme des poids des objets non rangés
+        for obj in listeObj2:
+            sumPoids += obj.poids
+        capBoite = cap_la_plus_proche(sumPoids, capBoites)
+        boite = Boite(capBoite , i) # Création d'un boite avec la cap. la plus proche de la somme des poids des objets
+        i += 1
+        for obj in listeObj2:
+            boite.ajouterObjet(obj) # Ajout des objets restants dans la boite (risque de rejet d'objets à cause de la contrainte de couleurs)
+        sumPoids = 0 # Somme des poids des objets dans la boite
+        for obj in boite.listeObj:
+            sumPoids += obj.poids
+        boite.cap = cap_la_plus_proche(sumPoids, capBoites) # Réadaptation de la capacité de la boite
+        for obj in boite.listeObj: # Suppression des objets déjà rangés dans des boites
+            listeObj2.remove(obj)
+        listeBoites.append(boite) # Ajout de la boite à la liste des boites
+        
+    sumCapResid = 0
+    for boite in listeBoites:
+        sumCapResid += boite.capResiduelle()
+    
+    tempsActuel = clock()
+    while ((tempsActuel - tempsDebut) < tempsLimite) and (sumCapResid > 0):
+        boiteLaPlusVide = listeBoites[0]
+        for boite in listeBoites: # Sélection de la boite la plus vide
+            if boiteLaPlusVide.capResiduelle() < boite.capResiduelle():
+                boiteLaPlusVide = boite
+            
+        for boite in listeBoites:
+            for obj in boite.listeObj:
+                if obj.poids == boiteLaPlusVide.capResiduelle() and (boite != boiteLaPlusVide): # On cherche un objet qui pourrait combler la boite
+                    if boiteLaPlusVide.ajouterObjet(obj) == True: # Ajout de l'objet avec succès ?
+                        boite.supprimerObjet(obj.id)
+                        sumPoids = 0 # Somme des poids des objets dans la boite
+                        for obj in boite.listeObj:
+                            sumPoids += obj.poids
+                        boite.cap = cap_la_plus_proche(sumPoids, capBoites) # Réadaptation de la capacité de la boite
+        tempsActuel = clock()
+        sumCapResid = 0
+        for boite in listeBoites:
+            sumCapResid += boite.capResiduelle()
+                        
+                        
+                    
+        
+            
         
     
+        
+    return (listeBoites , sumCapResid)
 
         
         
@@ -146,11 +224,4 @@ def heur_naive_trie_selon_poids(listeObj , capBoites):
     
     
     
-    #===========================================================================
-    # for obj in listeObj:
-    #     print(obj)
-    # 
-    # for boite in boites:
-    #     print(boite)
-    #     
-    #===========================================================================
+
--- src/tests.py
+++ src/tests.py
@@ -3,19 +3,23 @@ from classes import *
 
 if __name__ == '__main__':
     
-    #===========================================================================
-    # for i in range(2,21):
-    #     for j in range(0,5):
-    #         nomBench = "../instances/bench_" + str(i) + "_" + str(j)
-    #         with open(nomBench , "r+") as bench:
-    #             for k in range(81):
-    #                 bench.readline()
-    #             bench.seek(bench.tell() - 1)
-    #             bench.write('13\t58')
-    # 
-    #===========================================================================
-    
+    capBoites , listeObj = chargerBench("../instances/bench_2_0")
+    boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
+    print("Capacité perdue = " + str(capPerdue1))
+    for boite in boites1:
+        print(boite)
+
     with open("resultats.csv" , "w+") as results:
+        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids\n")
+        capBoites , listeObj = chargerBench("../instances/test0")
+        boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
+        boites2 , capPerdue2 = heur_naive_bis(listeObj, capBoites)
+        boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
+        boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites , 0.1)
+        newLine = "test0," + str(capPerdue1) + \
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "\n"
+        results.write(newLine)
+        
         for i in range(2,21):
             for j in range(0,5):
                 nomBench = "../instances/bench_" + str(i) + "_" + str(j)
@@ -23,9 +27,7 @@ if __name__ == '__main__':
                 boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
                 boites2 , capPerdue2 = heur_naive_bis(listeObj, capBoites)
                 boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
+                boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites , 0.1)
                 newLine = 'bench_' + str(i) + "_" + str(j) + "," + str(capPerdue1) + \
-                    "," + str(capPerdue2) + "," + str(capPerdue3) + ",\n"
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "\n"
                 results.write(newLine)
-
-
-    
