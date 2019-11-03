dc8ec5b8c808d0cab7460c21b91266d7dd52831d
Romaric Doumenc <Romaric.Doumenc@etu.univ-valenciennes.fr>
2019-11-03 11:02:01 +0100

    Ajout d'une heuristique

--- src/heuristiques.py
+++ src/heuristiques.py
@@ -299,21 +299,101 @@ def heur_naive_trie_selon_poids_depl_obj_bis(listeObj , capBoites , tempsLimite=
         
     return (listeBoites , sumCapResid)
 
-
-
-#===============================================================================
-# def heur_first_fit(listeObj , capBoites):
-#     """Heuristique utilisant la méthode du first-fit : on trie loe objets par poids décroissant , puis on les range dans
-#     l'ordre de la liste. Chaque objet traité est rangé"""
-#     listeBoites = []
-#     listeObj2 = listeObj.copy() # Copie de la liste d'objets afin de ne pas altérer la liste originale
-#     i = 0 # itérateur pour l'id des boites
-#     listeObj2 = trier_objets_selon_poids(listeObj2)
-#===============================================================================
+def heur_naive_trie_selon_poids_depl_obj_ter(listeObj , capBoites , tempsLimite=1):
+    """Même heuristique que précédemment sauf que l'étape de placer les objets de taille égale à un capacité d'une boite ; a été remise ici
+    et les objets sont triés selon leur poids par ordre décroissant.
+    Ensuite on essaie de transférer des objets dans des boites qui pourraient être comblées par ces objets là.
+    Et on redimensionne (si possible) la boite à qui on a volé un objet par un des types de boites existants afin de réduire la 
+    capacité perdue. La limite de temps est en secondes. (par défaut = 1 seconde)"""
+    
+    tempsDebut = clock()
+    
+    listeBoites = []
+    listeObj2 = listeObj.copy() # Copie de la liste d'objets afin de ne pas altérer la liste originale
+    i = 0 # itérateur pour l'id des boites
+    listeObj2 = trier_objets_selon_poids(listeObj2)
+    
+    # L'étape de placer des objets qui sont égaux à une capacité de boite donné a été rétablie dans cette heuristique.
+    for obj in listeObj2:
+        try:
+            capBoites.index(obj.poids) # Si le poids de l'objet coincide avce la capacité d'une boite
+            boite = Boite(obj.poids,i) # Création de la boite appropriée
+            boite.ajouterObjet(obj) # Ajout de l'objet à cette boite
+            listeBoites.append(boite) # Ajout de la boite à la liste des boites
+            i += 1
+        except ValueError:
+            pass
+        
+    for boite in listeBoites: # Suppression des objets déjà rangés dans des boites
+        for obj in boite.listeObj:
+            listeObj2.remove(obj)
+    
+     
+    while listeObj2 != []:
+        sumPoids = 0 # Somme des poids des objets non rangés
+        for obj in listeObj2:
+            sumPoids += obj.poids
+        capBoite = cap_la_plus_proche(sumPoids, capBoites)
+        boite = Boite(capBoite , i) # Création d'un boite avec la cap. la plus proche de la somme des poids des objets
+        i += 1
+        for obj in listeObj2:
+            boite.ajouterObjet(obj) # Ajout des objets restants dans la boite (risque de rejet d'objets à cause de la contrainte de couleurs)
+        sumPoids = 0 # Somme des poids des objets dans la boite
+        for obj in boite.listeObj:
+            sumPoids += obj.poids
+        boite.cap = cap_la_plus_proche(sumPoids, capBoites) # Réadaptation de la capacité de la boite
+        for obj in boite.listeObj: # Suppression des objets déjà rangés dans des boites
+            listeObj2.remove(obj)
+        listeBoites.append(boite) # Ajout de la boite à la liste des boites
+        
+    sumCapResid = 0
+    for boite in listeBoites:
+        sumCapResid += boite.capResiduelle()
+    
+    tempsActuel = clock()
+    while ((tempsActuel - tempsDebut) < tempsLimite) and (sumCapResid > 0):
+        # boucle qui s'arrêtera lortsque la limite de temps sera dépassée
+        boiteLaPlusVide = listeBoites[0]
+        for boite in listeBoites: # Sélection de la boite la plus vide
+            if boiteLaPlusVide.capResiduelle() < boite.capResiduelle():
+                boiteLaPlusVide = boite  
+        objetsCandidats = []
+        for boite in listeBoites:
+            for obj in boite.listeObj: # Recherche des objets candidats pouvant rentrer dans la boite vide
+                if obj.poids <= boiteLaPlusVide.capResiduelle() and boite != boiteLaPlusVide:
+                    objetsCandidats.append(obj)
+        objetsCandidats = trier_objets_selon_poids(objetsCandidats) # Tri de ces objets par ordre décroissnt de poids
+        objAjoute = None
+        for obj in objetsCandidats: # Tentative d'ajout d'un de ces objets dans la boite la plus vide
+            if boiteLaPlusVide.ajouterObjet(obj) == True:
+                objAjoute = obj
+                break # Sortie de la boucle for
+        if objAjoute != None:
+            for boite in listeBoites:
+                try:
+                    boite.listeObj.index(obj) # Est-ce que l'objet ajouté vient de cette boite ?
+                    for obj in boite.listeObj:
+                        sumPoids += obj.poids
+                    boite.cap = cap_la_plus_proche(sumPoids, capBoites) # Si oui , Réadaptation de la capacité de la boite
+                except ValueError:
+                    pass
+        tempsActuel = clock()
+        sumCapResid = 0
+        for boite in listeBoites:
+            sumCapResid += boite.capResiduelle()
+                        
+                        
+                    
+        
+            
+        
+    
+        
+    return (listeBoites , sumCapResid)
     
           
 if __name__ == '__main__':
-    capBoites , listeObj = chargerBench("../instances/test0")
+    capBoites , listeObj = chargerBench("../instances2/bench_20_10")
     boites1 , capPerdue1 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites)
     print("Capacité perdue = " + str(capPerdue1))
     for boite in boites1:
--- src/tests.py
+++ src/tests.py
@@ -1,19 +1,24 @@
+from classes import Boite , Objet , chargerBench
 from heuristiques import *
-from classes import *
+
+
 
 if __name__ == '__main__':
+    
+    debut = clock()
 
     with open("resultats.csv" , "w+") as results:
-        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis\n")
+        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis,heur_naive_triee_selon_poids_depl_obj_ter\n")
         capBoites , listeObj = chargerBench("../instances/test0")
         boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
         boites2 , capPerdue2 = heur_naive_bis(listeObj, capBoites)
         boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
         boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites)
         boites5 , capPerdue5 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites)
+        boites6 , capPerdue6 = heur_naive_trie_selon_poids_depl_obj_ter(listeObj, capBoites)
 
         newLine = "test0," + str(capPerdue1) + \
-                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) +"\n"
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) + "," + str(capPerdue6) + "\n"
         results.write(newLine)
         
         for i in range(2,21):
@@ -25,21 +30,26 @@ if __name__ == '__main__':
                 boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
                 boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites , 0.1)
                 boites5 , capPerdue5 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites , 0.1)
+                boites6 , capPerdue6 = heur_naive_trie_selon_poids_depl_obj_ter(listeObj, capBoites , 0.1)
                 newLine = 'bench_' + str(i) + "_" + str(j) + "," + str(capPerdue1) + \
-                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) +"\n"
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) + "," + str(capPerdue6) + "\n"
                 results.write(newLine)
+        # Calcul de la moyenne de la capacité perdue lorsque le CSV est exporté dans un tableur
+        results.write("Moyenne,=MOYENNE(B2:B97),=MOYENNE(C2:C97),=MOYENNE(D2:D97),=MOYENNE(E2:E97),=MOYENNE(F2:F97),=MOYENNE(G2:G97)")
+
                 
     with open("resultats2.csv" , "w+") as results:
-        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis\n")
-        capBoites , listeObj = chargerBench("../instances/test0")
+        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis,heur_naive_triee_selon_poids_depl_obj_ter\n")
+        capBoites , listeObj = chargerBench("../instances2/test0")
         boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
         boites2 , capPerdue2 = heur_naive_bis(listeObj, capBoites)
         boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
         boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites)
         boites5 , capPerdue5 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites)
+        boites6 , capPerdue6 = heur_naive_trie_selon_poids_depl_obj_ter(listeObj, capBoites)
 
         newLine = "test0," + str(capPerdue1) + \
-                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) +"\n"
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) + "," + str(capPerdue6) + "\n"
         results.write(newLine)
         
         for i in range(2,21):
@@ -51,9 +61,15 @@ if __name__ == '__main__':
                 boites3 , capPerdue3 = heur_naive_trie_selon_poids(listeObj, capBoites)
                 boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites , 0.1)
                 boites5 , capPerdue5 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites , 0.1)
+                boites6 , capPerdue6 = heur_naive_trie_selon_poids_depl_obj_ter(listeObj, capBoites , 0.1)
                 newLine = 'bench_' + str(i) + "_" + str(j) + "," + str(capPerdue1) + \
-                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) +"\n"
+                    "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) + "," + str(capPerdue6) + "\n"
                 results.write(newLine)
+        # Calcul de la moyenne de la capacité perdue lorsque le CSV est exporté dans un tableur
+        results.write("Moyenne,=MOYENNE(B2:B382),=MOYENNE(C2:C382),=MOYENNE(D2:D382),=MOYENNE(E2:E382),=MOYENNE(F2:F382),=MOYENNE(G2:G382)")
                 
-
-    print("Résultats enregistrés dans le fichier resultats.csv. Ouvrez ce fichier avec un tableur pour exploiter les résultats obtenus.")
+    fin = clock()
+    print("Résultats enregistrés dans les fichiers resultats.csv et resultats2.csv. Ouvrez ce fichier avec un tableur pour exploiter les résultats obtenus.")
+    print("Exécuté en " + str(fin-debut) + " secondes")
+    
+    
