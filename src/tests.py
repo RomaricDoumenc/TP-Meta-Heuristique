88861929b5980e853034c885c79a838a703c7d80
Romaric Doumenc <Romaric.Doumenc@etu.univ-valenciennes.fr>
2019-11-07 18:22:38 +0100

    Ajout d'une heuristique prenant les objets 2 par 2 et les met dans une boite de type appropriée. Résultats nettement meilleurs.

--- src/tests.py
+++ src/tests.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     tempsParBench = 0.01 # Temps max de résolution par bench (en secondes) sauf pour la méthode aléatoire.
 
     with open("resultats.csv" , "w+") as results:
-        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis,heur_naive_triee_selon_poids_depl_obj_ter\n")
+        results.write(",heur_naive,heur_naive_bis,heur_naive_triee_selon_poids,heur_naive_triee_selon_poids_depl_obj,heur_naive_triee_selon_poids_depl_obj_bis,heur_naive_triee_selon_poids_depl_obj_ter,heur_naive_triee_selon_poids_depl_obj_quad\n")
         capBoites , listeObj = chargerBench("../instances/test0")
         boites1 , capPerdue1 = heur_naive(listeObj, capBoites)
         boites2 , capPerdue2 = heur_naive_bis(listeObj, capBoites)
@@ -34,13 +34,14 @@ if __name__ == '__main__':
                 boites4 , capPerdue4 = heur_naive_trie_selon_poids_depl_obj(listeObj, capBoites)
                 boites5 , capPerdue5 = heur_naive_trie_selon_poids_depl_obj_bis(listeObj, capBoites)
                 boites6 , capPerdue6 = heur_naive_trie_selon_poids_depl_obj_ter(listeObj, capBoites)
-                boites7 , capPerdue7 = repeter_heur_alea(listeObj, capBoites)
+                boites7 , capPerdue7 = heur_naive_trie_selon_poids_depl_obj_quad(listeObj,capBoites)
                 newLine = "bench_"  + str(i) + "_" + str(j) + "," + str(capPerdue1) + \
                     "," + str(capPerdue2) + "," + str(capPerdue3) + "," + str(capPerdue4) + "," + str(capPerdue5) + "," + str(capPerdue6) + \
                     "," +  str(capPerdue7) + "\n"
                 results.write(newLine)
+                print("bench_{}_{} testé.".format(i,j))
         # Calcul de la moyenne de la capacité perdue lorsque le CSV est exporté dans un tableur
-        results.write("Moyenne,=MOYENNE(B2:B97),=MOYENNE(C2:C97),=MOYENNE(D2:D97),=MOYENNE(E2:E97),=MOYENNE(F2:F97),=MOYENNE(G2:G97)")
+        results.write("Moyenne,=MOYENNE(B2:B97),=MOYENNE(C2:C97),=MOYENNE(D2:D97),=MOYENNE(E2:E97),=MOYENNE(F2:F97),=MOYENNE(G2:G97),=MOYENNE(H2:H97)")
 
                 
     # with open("resultats2.csv" , "w+") as results:
